b86e309ce904ce04640f00e14c0f4084d6e860f1
danferdiansyah <danferdianstyle@gmail.com>
2025-03-23 18:16:55 +0700

    fix: notification bug imports

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -6,7 +6,6 @@ use rocket::http::Status;
 use crate::model::notification::Notification;
 use crate::model::product::Product;
 use crate::model::subscriber::Subscriber;
-use crate::repository::subscriber::Subscriber::Repository;
 use crate::repository::subscriber::SubscriberRepository;
 
 pub struct NotificationService;
